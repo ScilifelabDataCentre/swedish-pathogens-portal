cf14180a84456a07439d57417782a3486d855f6b
Senthilkumar Panneerselvam <senthilkumar.panneerselvam@scilifelab.se>
2025-07-29 11:18:58 +0200

    FREYA-1529 create basic template

--- pathogens_portal/settings.py
+++ pathogens_portal/settings.py
@@ -66,7 +66,9 @@ ROOT_URLCONF = "pathogens_portal.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            BASE_DIR / "pathogens_portal" / "templates"
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -124,6 +126,10 @@ USE_TZ = True  # enable timezone support
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "pathogens_portal" / "static"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
