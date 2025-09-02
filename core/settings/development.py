3a68c9b4cda7f78c17ab06742fcb8aa7640ae5b2
Ziip-dev <62331138+Ziip-dev@users.noreply.github.com>
2025-09-02 17:06:01 +0200

    chore(deps): add `django-extensions` for development

--- core/settings/development.py
+++ core/settings/development.py
@@ -11,6 +11,12 @@ DEBUG = True
 
 ADMIN_URL = "admin/"
 
+
+# DEVELOPMENT APPS
+# ------------------------------------------------------------------------------
+INSTALLED_APPS += ["django_extensions"]  # noqa: F405
+
+
 # SECURITY
 # ------------------------------------------------------------------------------
 CSRF_TRUSTED_ORIGINS = ["http://localhost:8000", "http://127.0.0.1:8000"]
