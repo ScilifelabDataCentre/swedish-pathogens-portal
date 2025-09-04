e2cacc1fe6bfdd2949601208e03bdfe3e05c777d
Senthilkumar Panneerselvam <senthil10@users.noreply.github.com>
2025-09-04 16:10:18 +0200

    Initialise utils directory, add and use TemplateView utility class (#11)
    * Initialise utils directory
    
    * Add base template view class
    
    * Use utils BaseTemplateView

---
+++ utils/views/_base_template_view.py
@@ -0,0 +1,28 @@
+from django.views.generic import TemplateView
+
+
+class BaseTemplateView(TemplateView):
+    """Base template view class
+
+    This class can be inherited by satic page view classes that only
+    renders a template by passing a context. Below defined attributes
+    can be set in the child classes to be added to the context.
+    """
+
+    title = ""
+    description = ""
+    extra_context = None
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        if self.title:
+            context["title"] = self.title
+
+        if self.description:
+            context["description"] = self.description
+
+        if self.extra_context is not None and isinstance(self.extra_context, dict):
+            context.update(self.extra_context)
+
+        return context
