25678ee04edc3f952ee268bc93b586c05920e4e6
Rickard Hammarén <rickard.hammaren@scilifelab.uu.se>
2025-09-10 14:43:31 +0200

    Minimal working start

--- pages/data_management/apps.py
+++ pages/data_management/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class DataManagementConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'data_management'
+    name = 'pages.data_management'
