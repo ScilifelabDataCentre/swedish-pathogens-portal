57dcc9ab172f4c2c583e6bf21850e0df9b13ff80
Kazi Jahurul Islam <jahurul.sust@gmail.com>
2025-09-15 09:59:13 +0200

    Add breadcrumb navigation system
    - Create breadcrumbs.html template with responsive design
    - Update base.html to include breadcrumb navigation
    - Enhance BaseTemplateView to support breadcrumb data
    - Add breadcrumb support to all about pages
    - Implement accessible breadcrumb navigation using Tailwind CSS

--- utils/views/_base_template_view.py
+++ utils/views/_base_template_view.py
@@ -12,6 +12,7 @@ class BaseTemplateView(TemplateView):
     title = ""
     description = ""
     extra_context = None
+    breadcrumbs = None
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -21,6 +22,9 @@ class BaseTemplateView(TemplateView):
 
         if self.description:
             context["description"] = self.description
+
+        if self.breadcrumbs is not None:
+            context["breadcrumbs"] = self.breadcrumbs
 
         if self.extra_context is not None and isinstance(self.extra_context, dict):
             context.update(self.extra_context)
