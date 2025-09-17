8d7dbe380f0d8dbc8001e6451694a84a322afeaa
Abdullah Aziz <mister.abdullah.aziz@gmail.com>
2025-09-18 00:41:57 +0200

    refactor(urls): simplify import statements for topic views

--- pages/topics/urls.py
+++ pages/topics/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-
-from . import views
+from .views import TopicListView, TopicDetailView
 
 app_name = "topics"
 
 urlpatterns = [
-    path("", views.TopicListView.as_view(), name="index"),
-    path("<slug:slug>/", views.TopicDetailView.as_view(), name="topic_detail"),
+    path("", TopicListView.as_view(), name="index"),
+    path("<slug:slug>/", TopicDetailView.as_view(), name="topic_detail"),
 ]
