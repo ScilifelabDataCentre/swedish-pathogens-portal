4a8d9df2231ed15322f091234a48d0da249ae74e
Senthilkumar Panneerselvam <5311007+senthil10@users.noreply.github.com>
2026-08-19 14:58:16 +0200

    Add fallback date if dashboard don't have data entry (#83)
    * Add fallback date if dashboard don't have data entry
    
    * Proper fallback

--- cms/pages/dashboard.py
+++ cms/pages/dashboard.py
@@ -5,6 +5,7 @@ from typing import TYPE_CHECKING, Any
 
 from django.db import models
 from django.http import HttpRequest
+from django.utils import timezone
 from django.utils.functional import cached_property
 from modelcluster.fields import ParentalKey
 from wagtail.admin.panels import FieldPanel, InlinePanel, MultiFieldPanel, PageChooserPanel
@@ -147,6 +148,13 @@ class DashboardPage(Page):
                 "headings in the content and displayed in a sidebar."
             ),
         ),
+        FieldPanel(
+            "first_published_at",
+            help_text=(
+                "The page creation date that will be displayed on the card, if the dashboard "
+                "doesn't have a corresponding data update timestamp."
+            ),
+        ),
     ]
 
     @property
@@ -170,8 +178,13 @@ class DashboardPage(Page):
 
     @property
     def dashboard_data_updated_at(self) -> date | None:
-        """Return the last updated timestamp for the dashboard data."""
-        return getattr(self.dashboard_data, "data_updated_at", None)
+        """Return the last updated timestamp for the dashboard data or the first published date."""
+        data_date = getattr(self.dashboard_data, "data_updated_at", None)
+        if data_date:
+            return data_date
+        if self.first_published_at:
+            return timezone.localdate(self.first_published_at)
+        return None
 
     def get_context(self, request: HttpRequest) -> dict[str, Any]:
         """Add DashboardData figures, CSV URL, and parent heading to template context."""
